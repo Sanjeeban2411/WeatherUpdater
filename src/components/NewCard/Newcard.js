22497b454b5f54ede96c501ab4ac372accec90aa
Sanjeeban Parasar <73850652+Sanjeeban2411@users.noreply.github.com>
2021-12-08 22:17:16 +0530

    Update Newcard.js

--- src/components/NewCard/Newcard.js
+++ src/components/NewCard/Newcard.js
@@ -12,32 +12,34 @@ export default function Newcard({ hideAdderCard, list, setlist }) {
   return (
     <div className={Styles.card}>
       {/* <form action="submit"> */}
-        <div className={Styles.nameHolder}>
-          <input
-            type="text"
-            className={Styles.input}
-            placeholder="Place"
-            onChange={(e) => {
-              setText(e.target.value);
-            }}
-          />
-        </div>
-        <div className={Styles.buttonCtn}>
+      <div className={Styles.nameHolder}>
+        <input
+          type="text"
+          className={Styles.input}
+          placeholder="Enter location"
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+        />
+      </div>
+      <div className={Styles.buttonCtn}>
         <button
           type="submit"
+          className={Styles.button1}
           onClick={() => {
             createCard();
           }}
         >
           Submit
         </button>
-      <button
-        onClick={() => {
-          hideAdderCard();
-        }}
+        <button
+          className={Styles.button2}
+          onClick={() => {
+            hideAdderCard();
+          }}
         >
-        Cancel
-      </button>
+          Cancel
+        </button>
         {/* </form> */}
       </div>
     </div>
